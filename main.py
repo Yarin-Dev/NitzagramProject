50bacda8b63c8886ed9b3922b1dc8d1588f7bfa6
Yarin-Dev <117749540+Yarin-Dev@users.noreply.github.com>
2025-02-06 18:23:10 +0200

    Added like, comment, share and a few more changes
    * Added *
    Added like, comments and share features & events.
    
    * Changed *
    Changed some of the code, so it looks smoother and readable.

--- main.py
+++ main.py
@@ -1,54 +1,90 @@
 import pygame
-from helpers import screen
+from helpers import screen, mouse_in_button, read_from_user, load_and_scale, put_like_sign, resolve_phone_number
 from constants import WINDOW_WIDTH, WINDOW_HEIGHT, BLACK
 from classes.Post import *
 from classes.ImagePost import *
 from classes.TextPost import *
+from buttons import *
+from classes.Comment import Comment
+
 
 def main():
     # Set up the game display, clock and headline
     pygame.init()
 
     # Change the title of the window
-    pygame.display.set_caption('Niggagram')
+    pygame.display.set_caption('Nitzagram')
 
     clock = pygame.time.Clock()
 
     # Set up background image
-    # Set up background image
-    background = pygame.image.load('Images/background.png')
-    background = pygame.transform.scale(background,
-                                        (WINDOW_WIDTH, WINDOW_HEIGHT))
+    background = load_and_scale('NitzagramProject-main/Images/background.png', WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    post1 = ImagePost("YuvalEl", "Israel", "my first post description", "images/ronaldo.jpg")
+    post1 = ImagePost("YuvalEl", "Israel", "my first post description", "NitzagramProject-main/images/ronaldo.jpg")
     post2 = TextPost("YuvalEl", "Israel", "and here is the second one", "Post Text", (255, 255, 255), (0, 0, 0))
     posts = [post1, post2]
-
+    
     index = 0
     running = True
     while running:
         # Grabs events such as key pressed, mouse pressed and so.
         # Going through all the events that happened in the last clock tick
+        current_post = posts[index]
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                if ((POST_X_POS  <= pos[0] <= POST_X_POS + POST_WIDTH) and (POST_Y_POS <= pos[1] <= POST_Y_POS + POST_HEIGHT)):
+                
+                # Check next post button click
+                if mouse_in_button(click_post_button, pos):
+                    
+                    # Move to the next one or restart if reached the last post
                     index += 1
                     if index == len(posts):
                         index = 0
 
+                # Check comment button click
+                elif mouse_in_button(comment_button, pos):
+                    
+                    # Ask for username and content
+                    username = read_from_user("Enter nickname:")
+                    text = read_from_user("Enter content: ")
+                    
+                    # Add the comment
+                    current_post.add_comment(Comment(username, text))
+                
+                # Check like button click
+                elif mouse_in_button(like_button, pos):
+                    
+                    # Add the like to the post
+                    current_post.add_like()
+                
+                # Check share button click
+                elif mouse_in_button(share_button, pos):
+                    share_to_num = read_from_user(title="Enter phone number to share to:")
+                    
+                    # Share the post to the number provided - If the number is a valid num
+                    if share_to_num.isdigit():
+                        current_post.share(resolve_phone_number(share_to_num))
+                    
         # Display the background, presented Image, likes, comments, tags and location(on the Image)
         screen.fill(BLACK)
         screen.blit(background, (0, 0))
-
+        current_post.display()
+        current_post.display_comments()
+        
+        # Put the red heart if user make a like to the post
+        if current_post.likes_counter > 0:
+            put_like_sign()
+        
         # Update display - without input update everything
-        posts[index].display()
         pygame.display.update()
 
         # Set the clock tick to be 60 times per second. 60 frames for second.
         clock.tick(60)
+        
     pygame.quit()
     quit()
 
