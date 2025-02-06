3b6c04c4af95fc0e29dc5fb90f327b2ab3899041
Yarin-Dev <117749540+Yarin-Dev@users.noreply.github.com>
2025-02-06 18:06:55 +0200

    Update TextPost.py
    Added share function.

--- classes/TextPost.py
+++ classes/TextPost.py
@@ -3,6 +3,7 @@ from classes.Post import *
 import pygame
 from helpers import screen
 from helpers import center_text
+import pywhatkit
 
 class TextPost(Post):
     def __init__(self, username: str, location: str, description: str, text: str, color: tuple, text_color: tuple):
@@ -19,3 +20,9 @@ class TextPost(Post):
         text = font.render(self.text, True, self.text_color)
         obj = center_text(1, text, 1)
         screen.blit(text, (obj.x, obj.y))
+        
+    def share(self, phone_num):
+        content = "**{}'s post from Nitzagram!**\n{} likes • {} shares • {} comments\n\n".format(self.username, self.likes_counter, self.share_counter, len(self.comments))
+        content += f"*{self.text}*\n{self.description}"
+        pywhatkit.sendwhatmsg_instantly(phone_num, content)
+        
