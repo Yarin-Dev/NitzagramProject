e8b8ab05c3f555e9b3298ba50ffe2e571d0723b9
Yarin-Dev <117749540+Yarin-Dev@users.noreply.github.com>
2025-02-06 18:05:26 +0200

    Update ImagePost.py
    Added share function,

--- classes/ImagePost.py
+++ classes/ImagePost.py
@@ -1,7 +1,8 @@
+import pygame
+import pywhatkit
+from helpers import screen, load_and_scale
 from constants import *
 from classes.Post import *
-import pygame
-from helpers import screen
 
 class ImagePost(Post):
     def __init__(self, username: str, location: str, description: str, image: str):
@@ -10,6 +11,11 @@ class ImagePost(Post):
 
     def display(self):
         super().display()
-        img = pygame.image.load(self.image)
-        image = pygame.transform.scale(img,(POST_WIDTH, POST_HEIGHT))
+        image = load_and_scale(self.image, POST_WIDTH, POST_HEIGHT)
         screen.blit(image, (POST_X_POS, POST_Y_POS))
+        
+    def share(self, phone_num):
+        self.add_share()
+        content = "**{}'s post from Nitzagram!**\n{} likes • {} shares • {} comments\n\n".format(self.username, self.likes_counter, self.share_counter, len(self.comments))
+        content += f"{self.description}"
+        pywhatkit.sendwhats_image(phone_num, self.image, content)
