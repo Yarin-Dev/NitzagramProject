180dd069fad24f7297d85dc0e8359b9f8106a955
Yarin-Dev <117749540+Yarin-Dev@users.noreply.github.com>
2025-02-06 18:29:34 +0200

    Added a few code notes

--- classes/Comment.py
+++ classes/Comment.py
@@ -17,13 +17,16 @@ class Comment:
         Displays the comment sorted by position_index.
         :return: None
         """
+        # Font & Text
         font = pygame.font.SysFont("chalkduster.ttf",
                                    20)
         text = font.render(self.nickname + ': ' + self.text, True, GREY)
         
+        # Text's positions
         text_x = DESCRIPTION_TEXT_X_POS + 20
         text_y = DESCRIPTION_TEXT_Y_POS + 30 * position_index
         
+        # Show on the screen with user's profile.
         user_profile = load_and_scale('NitzagramProject-main/Images/user-profile.png', 30, 27)
         screen.blit(text, (text_x, text_y))
         screen.blit(user_profile, (text_x - 30, text_y - 7))
